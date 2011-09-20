d4f92230bdb18fbd2d1ca1ceb48e8482459f6326
Ryosuke Nakai <twisted.mind@voluntas.net>
2011-09-21 00:59:04 +0900

    Import

---
+++ snowflake/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'snowflake.views.home', name='home'),
+    # url(r'^snowflake/', include('snowflake.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve', kwargs={"insecure": True}),
+)
