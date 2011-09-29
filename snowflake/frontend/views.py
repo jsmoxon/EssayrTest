56bd7b63fcfcb200db636b627db3036090839f2d
Jack Moxon <jsmoxon@gmail.com>
2011-09-29 11:23:53 -0700

    launch ready

--- snowflake/frontend/views.py
+++ snowflake/frontend/views.py
@@ -6,26 +6,30 @@ from frontend.models import Entry, Response
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+def homepage(request):
+    return render_to_response('blogonymous/homepage.html')
+
+
 def yeah(request):
-    return render_to_response('first.html')
+    return render_to_response('blogonymous/first.html')
 
 def home(request):
     entries = Response.objects.order_by('question')
     p = Entry.objects.order_by('answers')
-    return render_to_response("home.html", {"entries": entries, "p": p})
+    return render_to_response("blogonymous/home.html", {"entries": entries, "p": p})
 
 def detail(request, entry_id):
     p = get_object_or_404(Entry, pk=entry_id)
-    return render_to_response('response_form.html', {"p": p}, context_instance=RequestContext(request))
+    return render_to_response('blogonymous/response_form.html', {"p": p}, context_instance=RequestContext(request))
 
 def submit(request, entry_id):
     p = get_object_or_404(Entry, pk=entry_id)
-    return render_to_response('submit.html', {"p": p}, context_instance=RequestContext(request))
+    return render_to_response('blogonymous/submit.html', {"p": p}, context_instance=RequestContext(request))
 
 def single(request, entry_id):
     p = get_object_or_404(Entry, pk=entry_id)
     q = p.response_set.order_by('votes')
-    return render_to_response('single.html', {'p': p, 'q':q}, context_instance=RequestContext(request))
+    return render_to_response('blogonymous/single.html', {'p': p, 'q':q}, context_instance=RequestContext(request))
 
 #submits a response to an entry
 def enter(request, entry_id):
@@ -49,11 +53,11 @@ def upvote(request, entry_id):
 def blog_post(request, entry_id, answer_id):
     p = get_object_or_404(Entry, pk=entry_id)
     q = get_object_or_404(Response, pk=answer_id)
-    return render_to_response('post.html', {"p":p, "q":q})
+    return render_to_response('blogonymous/post.html', {"p":p, "q":q})
     
 
 def question(request):
-    return render_to_response('question.html', context_instance=RequestContext(request))
+    return render_to_response('blogonymous/question.html', context_instance=RequestContext(request))
 
 def question_submit(request):
     new = Entry.objects.create(question=request.POST['question'])
@@ -63,4 +67,4 @@ def question_submit(request):
     return HttpResponseRedirect(reverse('frontend.views.home'))
 
 def about(request):
-    return render_to_response('about.html')
+    return render_to_response('blogonymous/about.html')
