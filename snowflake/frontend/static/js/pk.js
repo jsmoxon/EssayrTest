8911f5eb7598f1b910295f8e8d489d18ce5423f0
ptrklly <ptrklly@gmail.com>
2011-10-27 22:31:53 -0700

    moved feedback jquery to pk.js

--- snowflake/frontend/static/js/pk.js
+++ snowflake/frontend/static/js/pk.js
@@ -1,5 +1,9 @@
  $(document).ready(function() {
 
-
+  <script type="text/javascript">
+  function feedbackPopup() {
+  window.open("/feedback/", "feedbackWindow", "status=1, height = 400, width = 400, resizable = 0")
+  }
+  </script>
 
  });
